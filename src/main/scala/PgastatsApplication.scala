5c125478d47322262d78bae37241b3c13d0b769a
Haris Beha <harisbeha@gmail.com>
2017-09-26 10:32:56 -0400

    Change main class to use scala

---
+++ src/main/scala/PgastatsApplication.scala
@@ -0,0 +1,11 @@
+package org.behatechconsultants.pgastats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+class PgastatsApplication
+object PgastatsApplication extends App {
+    SpringApplication.run(classOf[PgastatsApplication]);
+}
